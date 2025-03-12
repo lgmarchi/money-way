34a75d4761a588874f75cec500d60cbb01072657
lgmarchi <lucasgmarchi@gmail.com>
2025-03-12 17:19:54 -0300

    feat(Auth): Enhance sign-up endpoint with database integration and state management

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,9 +1,12 @@
 use actix_web::{
+    HttpResponse,
     Responder,
     post,
     web,
 };
 use serde::Deserialize;
+
+use crate::AppState;
 
 #[derive(Deserialize, Debug)]
 struct SignUpRequest {
@@ -14,7 +17,30 @@ struct SignUpRequest {
 }
 
 #[post("/auth/sign-up")]
-pub async fn sign_up(data: web::Json<SignUpRequest>) -> impl Responder {
+pub async fn sign_up(
+    state: web::Data<AppState>,
+    data: web::Json<SignUpRequest>,
+) -> impl Responder {
+    // Lock the mutex to get the database pool
+    let db_pool = state.db.lock().await;
+
+    // Get a connection from the pool
+    match sqlx::query!(
+        "INSERT INTO users (email, password) VALUES (?, ?)",
+        data.email,
+        data.password
+    )
+    .execute(&*db_pool) // Deref to access Pool<MySql>
+    .await
+    {
+        Ok(_) => {
+            HttpResponse::Ok().body(format!("User {} signed up!", data.email));
+        }
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Error: {}", e));
+        }
+    }
+
     format!("Sign Up: {:?}", data)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use actix_web::{
     App,
     HttpServer,
+    web,
 };
 
 mod controllers;
 
+struct AppState {
+    db: tokio::sync::Mutex<sqlx::MySqlPool>,
+}
+
+const DATABASE_URL: &str = "DATABASE_URL";
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenvy::dotenv().ok();
+
+    let state: web::Data<AppState> = web::Data::new(AppState {
+        db: tokio::sync::Mutex::new(
+            sqlx::MySqlPool::connect(DATABASE_URL).await.unwrap(),
+        ),
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(state.clone())
             .service(controllers::auth::sign_up)
             .service(controllers::auth::sign_in)
     })
