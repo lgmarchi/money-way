aa1a39150420b1ef0fda4e72dcaa7f31509ea53a
lgmarchi <lucasgmarchi@gmail.com>
2025-03-12 16:45:25 -0300

    feat(Auth): Add sign-up endpoint with request validation using serde

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,11 +1,21 @@
 use actix_web::{
     Responder,
     post,
+    web,
 };
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct SignUpRequest {
+    email: String,
+    password: String,
+    first_name: String,
+    last_name: String,
+}
 
 #[post("/auth/sign-up")]
-pub async fn sign_up() -> impl Responder {
-    "Sign Up"
+pub async fn sign_up(data: web::Json<SignUpRequest>) -> impl Responder {
+    format!("Sign Up: {:?}", data)
 }
 
 #[post("/auth/sign-in")]
